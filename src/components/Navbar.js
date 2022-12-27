c568ffeb1449a891bc3b51b0c7a071b93ac1eec8
ijuso <72605682+ijuso@users.noreply.github.com>
2022-12-27 11:37:58 +0200

    navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,2 +1,14 @@
+ import "../styles/nav.css"
+
 export default function Navbar() {
+    return (
+        <div>
+        <ul className="navList">  
+            <li className="listItem"><a className="listItemText" href="#About">Home</a></li>  
+            <li className="listItem"><a className="listItemText" href="#Contact">About</a></li>  
+            <li className="listItem"><a className="listItemText" href="#Projects">FAQ</a></li>  
+            <li className="listItem"><a className="listItemText" href="#Skills">Contact</a></li>
+        </ul>
+    </div>
+    )
 }
